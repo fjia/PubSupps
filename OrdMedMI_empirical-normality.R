18c733ffcae3b7cba5c8b5b7c219fc8c8f4d5c65
fjia <fanjia2015@gmail.com>
2022-09-24 23:32:14 -0700

    Add files via upload

--- OrdMedMI_empirical-normality.R
+++ OrdMedMI_empirical-normality.R
@@ -4,28 +4,28 @@
 library(moments)
 n <- 10000
 
+b9 <- rep(NA, n)
 b10 <- rep(NA, n)
-b11 <- rep(NA, n)
 for (i in 1:n) {
   gama0 <- rnorm(1,0,1)
   gama1 <- rnorm(1,0,1)
-  b10[i] <- exp(gama0 + gama1)/(1+exp(gama0 + gama1)) - exp(gama0)/(1+exp(gama0))
-  b11[i] <- pnorm(gama0 + gama1) - pnorm(gama0)
+  b9[i] <- exp(gama0 + gama1)/(1+exp(gama0 + gama1)) - exp(gama0)/(1+exp(gama0))
+  b10[i] <- pnorm(gama0 + gama1) - pnorm(gama0)
 }
 
 par(mfrow = c(1,2))
+hist(b9)
 hist(b10)
-hist(b11)
+
+skewness(b9)
+kurtosis(b9)
 
 skewness(b10)
 kurtosis(b10)
 
-skewness(b11)
-kurtosis(b11)
+qqnorm(b9, pch = 1, frame = FALSE)
+qqline(b9, col = "steelblue", lwd = 2)
 
 qqnorm(b10, pch = 1, frame = FALSE)
 qqline(b10, col = "steelblue", lwd = 2)
-
-qqnorm(b11, pch = 1, frame = FALSE)
-qqline(b11, col = "steelblue", lwd = 2)
 
