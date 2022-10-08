5c84bab11199e335db279b0247dd391a16118f90
fjia <fanjia2015@gmail.com>
2022-10-07 18:42:56 -0700

    Update OrdMedMI_empirical-normality.R

--- OrdMedMI_empirical-normality.R
+++ OrdMedMI_empirical-normality.R
@@ -4,19 +4,18 @@
 library(moments)
 n <- 10000
 
-b9 <- rep(NA, n)
+b7 <- rep(NA, n)
 for (i in 1:n) {
   gama0 <- rnorm(1,0,1)
   gama1 <- rnorm(1,0,1)
-  b9[i] <- exp(gama0 + gama1)/(1+exp(gama0 + gama1)) - exp(gama0)/(1+exp(gama0))
+  b7[i] <- exp(gama0 + gama1)/(1+exp(gama0 + gama1)) - exp(gama0)/(1+exp(gama0))
 }
 
 par(mfrow = c(1,2))
-hist(b9)
+hist(b7)
 
-skewness(b9)
-kurtosis(b9)
+skewness(b7)
+kurtosis(b7)
 
-qqnorm(b9, pch = 1, frame = FALSE)
-qqline(b9, col = "steelblue", lwd = 2)
-
+qqnorm(b7, pch = 1, frame = FALSE)
+qqline(b7, col = "steelblue", lwd = 2)
